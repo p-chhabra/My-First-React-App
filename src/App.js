8e958e7c7561798e293509d09b112dca55ac018f
Prathmesh Chhabra <p.chhabra2002@gmail.com>
2022-04-27 02:01:42 +0530

    Expenses Section implemented

--- src/App.js
+++ src/App.js
@@ -1,62 +1,10 @@
-import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from "./Components/Expenses";
 
 function App() {
-  const expenses = [
-    {
-      title: "Car Insurance",
-      price: "$289.67",
-      date: new Date(2021, 7, 18),
-    },
-    {
-      title: "Life Insurance",
-      price: "$1000.00",
-      date: new Date(2022, 3, 22),
-    },
-    {
-      title: "House Insurance",
-      price: "$2500.00",
-      date: new Date(2022, 4, 26),
-    },
-    {
-      title: "Job Insurance",
-      price: "$375.00",
-      date: new Date(2020, 2, 3),
-    },
-    {
-      title: "Health Insurance",
-      price: "$480.50",
-      date: new Date(2019, 7, 13),
-    },
-  ];
-
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        price={expenses[0].price}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        price={expenses[1].price}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        price={expenses[2].price}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        price={expenses[3].price}
-        date={expenses[3].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[4].title}
-        price={expenses[4].price}
-        date={expenses[4].date}
-      ></ExpenseItem>
+      <Expenses></Expenses>
     </div>
   );
 }
---
+++ src/Components/Expenses.js
@@ -0,0 +1,69 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      price: "$289.67",
+      date: new Date(2021, 7, 18),
+    },
+    {
+      id: "e2",
+      title: "Life Insurance",
+      price: "$1000.00",
+      date: new Date(2022, 3, 22),
+    },
+    {
+      id: "e3",
+      title: "House Insurance",
+      price: "$2500.00",
+      date: new Date(2022, 4, 26),
+    },
+    {
+      id: "e4",
+      title: "Job Insurance",
+      price: "$375.00",
+      date: new Date(2020, 2, 3),
+    },
+    {
+      id: "e5",
+      title: "Health Insurance",
+      price: "$480.50",
+      date: new Date(2019, 7, 13),
+    },
+  ];
+
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={expenses[0].title}
+        price={expenses[0].price}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        price={expenses[1].price}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        price={expenses[2].price}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        price={expenses[3].price}
+        date={expenses[3].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[4].title}
+        price={expenses[4].price}
+        date={expenses[4].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default Expenses;
